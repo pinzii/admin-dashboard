4114ca8af165c60b922ebd5a8df4044c73bfb7ce
pinzii <fpinzon514@gmail.com>
2025-04-09 13:01:16 -0500

    Creación de componentes login, register, dashboard y layout con rutas básicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '',
+      redirectTo: '/login', 
+      pathMatch: 'full' 
+    },
+    {
+        path: 'login',
+        loadComponent: () => 
+            import('./pages/login.component').then(m => m.LoginComponent),
+    },
+    {
+        path: 'register',
+        loadComponent: () => 
+            import('./pages/register.component').then(m => m.RegisterComponent),
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => 
+            import('./pages/dashboard.component').then(m => m.DashboardComponent),
+        children: [
+            {
+                path: '',
+                loadComponent: () =>
+                    import('./pages/dashboard.component').then(m => m.DashboardComponent),
+            },
+        ],
+    },
+];
