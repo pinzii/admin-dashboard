5f0436ed2023f80e47b9500e643d23be11424f7f
pinzii <fpinzon514@gmail.com>
2025-04-22 22:03:47 -0500

    Agrega tarjetas con KPIs al dashboard y estilos

--- src/app/pages/dashboard.component.ts
+++ src/app/pages/dashboard.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [
-   
+   CommonModule,
+   MatCardModule,
+   MatIconModule
   ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
 
+  kpis = [
+    {title: 'Usuarios activos',
+     value: 120,
+     icon:'person' 
+    },
+    {title: 'Ventas hoy',
+      value: '$1.250',
+      icon: 'shopping_cart'      
+    },
+    {title: 'Visitas web',
+      value: '5230',
+      icon: 'visibility'      
+    },
+    {title: 'Tickets abiertos',
+      value: '14',
+      icon: 'support_agent'      
+    },
+
+  ];
 }
